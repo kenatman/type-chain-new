e2281c2ec9abfc9f47c6f518a04ab79e75d9ce58
Yunsu <kenatman0723@gmail.com>
2022-05-24 17:50:51 +0900

    블록체인 셋업

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-import { init, exit } from "./myPackage";
+interface IBlock {
+  prevHash: string;
+  height: number;
+  data: string;
+  hash: string;
+}
 
-init({ debug: true, url: "xxx" });
-exit(2);
+class Block implements IBlock {
+  public hash: string;
+
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return toHash;
+  }
+}
