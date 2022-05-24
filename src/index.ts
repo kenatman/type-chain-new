94cafd9bf0aac98d8cccc5655cb18bdec5a28ef6
Yunsu <kenatman0723@gmail.com>
2022-05-24 20:49:10 +0900

    블록체인 로직추가

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+
 interface IBlock {
   prevHash: string;
   height: number;
@@ -18,6 +20,34 @@ class Block implements IBlock {
 
   static calculateHash(prevHash: string, height: number, data: string) {
     const toHash = `${prevHash}${height}${data}`;
-    return toHash;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class BlockChain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash() {
+    if (this.blocks.length === 0) return '';
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+
+  public addBlock(data: string) {
+    const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+    this.blocks.push(newBlock);
+  }
+
+  public getBlocks() {
+    return [...this.blocks]; // 불변성을 지키며 리턴해야 원본을 변경 못한다.
+  }
+}
+
+const blockChain = new BlockChain();
+blockChain.addBlock('first');
+blockChain.addBlock('second');
+blockChain.addBlock('third');
+
+console.log(blockChain.getBlocks());
